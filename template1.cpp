c5c58db2fd856ecd9dee2848b63a11126c5ec238
Madhav Kumar <78339236+Madhav-MKNC@users.noreply.github.com>
2023-05-06 01:53:02 +0530

    Create template1.cpp

---
+++ template1.cpp
@@ -0,0 +1,27 @@
+// author: Madhav Kumar (https://github.com/madhav-mknc/)
+
+#include <bits/sdtc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define for(i,a,b) for (int i = a; i <= b; i++)
+
+
+
+
+
+int main(){
+    ios:sync_with_stdio(0);
+    cin.tie(0);
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+}
